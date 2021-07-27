1b997d46de336c8d6264207cf6fa00f13fb9a709
Ethan Lee <63252912+ethethanlee@users.noreply.github.com>
2021-07-26 21:46:46 -0700

    pulling grades out of a dictionary w/o list comp

--- HW12.py
+++ HW12.py
@@ -1,1 +1,21 @@
-#hello
+records = {"tim": {('CS','51P'): 90, ('CS','62'): 92, ('PE','90'): 87, ('Econ','51'): 97}, 
+"bob": {('CS','51P'): 78, ('Bio','52'): 94, ('PE','18'): 60, ('Math','101'): 95, ('Math','60'): 82}}
+
+def avg_grade(book,student):
+    average = 0
+    for y in records[student].values():
+        average += y
+    average = average / len(records[student])
+    return(average)
+
+
+print(avg_grade(records,"tim"))
+
+
+
+
+
+
+
+
+#average + x for x in book.values():
