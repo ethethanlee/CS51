16e8a549a1072e225b09d880b9bfbdc15a67df55
Ethan Lee <63252912+ethethanlee@users.noreply.github.com>
2021-07-27 10:35:13 -0700

    pull numbers out a dictionary using list comp

--- HW12.py
+++ HW12.py
@@ -1,18 +1,25 @@
+from functools import reduce
+
 records = {"tim": {('CS','51P'): 90, ('CS','62'): 92, ('PE','90'): 87, ('Econ','51'): 97}, 
 "bob": {('CS','51P'): 78, ('Bio','52'): 94, ('PE','18'): 60, ('Math','101'): 95, ('Math','60'): 82}}
 
 def avg_grade(book,student):
     average = 0
-    for y in records[student].values():
+    for y in book[student].values():
         average += y
-    average = average / len(records[student])
+    average = average / len(book[student])
     return(average)
 
 
 print(avg_grade(records,"tim"))
 
 
+def average_grade(boo,studen):
+    average = 0
+    average = reduce(lambda x,y: x + y, [average + y for y in boo[studen].values()])/len(boo[studen])
+    return(average)
 
+print(average_grade(records,"tim"))
 
 
 
