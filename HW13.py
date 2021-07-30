a330392ed90ad111abba09e643cc8941fc2419e2
Ethan Lee <63252912+ethethanlee@users.noreply.github.com>
2021-07-30 12:10:13 -0700

    opening csv file

---
+++ HW13.py
@@ -0,0 +1,8 @@
+
+# def read_records(filename):
+
+import csv
+with open('CS51.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))
