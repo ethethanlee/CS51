ed5001c0c48ff311a14ab50b9571bf7257e1e320
Ethan Lee <63252912+ethethanlee@users.noreply.github.com>
2021-07-30 18:11:08 -0700

    dictionary without classes

--- HW13.py
+++ HW13.py
@@ -4,12 +4,23 @@
 import csv
 
 def read_records(filename):
-    with open(filename, newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        ceesv = ''
-        for row in spamreader:
-            ceesv = ceesv + ', '.join(row) + '\n'
-    return ceesv
+    with open(filename) as csvfile:
+        ceereader = csv.reader(csvfile, delimiter=',')
+        diction = {}
+        for column in ceereader:
+            for x in range(7):
+                if column[x] == column[0]:
+                    pass
+                elif len(column[0]) > 2:
+                    # diction[column[0]] = column[x]
+                    diction.setdefault(column[0], [])
+                    diction[column[0]].append(column[x])
+        return diction
+
+    #     ceesv = ''
+    #     for row in spamreader:
+    #         ceesv = ceesv + ', '.join(row) + '\n'
+    # return ceesv
 
 
 
