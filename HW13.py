007b694091b05c96317bae1692eeb0470a513741
Ethan Lee <63252912+ethethanlee@users.noreply.github.com>
2021-07-31 14:07:36 -0700

    read csv record for CS classes

--- HW13.py
+++ HW13.py
@@ -1,27 +1,24 @@
-
-
-
 import csv
-
 def read_records(filename):
     with open(filename) as csvfile:
         ceereader = csv.reader(csvfile, delimiter=',')
         diction = {}
+        ces = []
         for column in ceereader:
             for x in range(7):
-                if column[x] == column[0]:
+                if 'CS' in column[x]:
+                    ces.append(column[x])
+                elif column[x] == column[0]:
                     pass
                 elif len(column[0]) > 2:
-                    # diction[column[0]] = column[x]
+                    hello = [ces[x-1] + ', ' + column[x]]
                     diction.setdefault(column[0], [])
-                    diction[column[0]].append(column[x])
+                    diction[column[0]].append(hello)
         return diction
+        
 
-    #     ceesv = ''
-    #     for row in spamreader:
-    #         ceesv = ceesv + ', '.join(row) + '\n'
-    # return ceesv
-
+# need to turn '90' into ['CS51P', '90']
+# need to bring back up the first instance of column from the for loop
 
 
 print(read_records('CS51.csv'))
